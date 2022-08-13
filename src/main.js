edf07b689b5086b45d1683a403773afba86fab1d
diogoizzo <izzoadv@gmail.com>
2022-08-12 22:24:05 -0300

    Javascript refatorado

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 const btn = document.getElementById("circulo");
 const video = document.getElementById("video");
-const frame = document.getElementById("divVideo");
-
-frame.addEventListener("click", (e) => {
-    if (e.target.parentElement === btn) video.play();
+btn.addEventListener("click", (e) => {
+    video.play();
 });
 video.onplay = () => (btn.style.display = "none");
 video.onpause = () => (btn.style.display = "block");
